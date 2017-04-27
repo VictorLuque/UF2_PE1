cd84e6b1e25bcf677ab0656d220774cbed0ad33b
victor <victor@victor-Lenovo-ideapad-100-15IBY>
2017-04-27 20:40:29 +0200

    test negatiu

---
+++ src/uf2_pe1/Testuf2_pe1negatiu.java
@@ -0,0 +1,27 @@
+package uf2_pe1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Testuf2_pe1negatiu {
+
+	@Test
+	public void test() {
+		Primordial prim = new Primordial();
+		Assert.assertEquals(2, prim.get_primordial(-2),0.005);
+		Assert.assertEquals(6, prim.get_primordial(-3),0.005);
+		Assert.assertEquals(6, prim.get_primordial(-4),0.005);
+		Assert.assertEquals(30, prim.get_primordial(-5),0.005);
+		Assert.assertEquals(210, prim.get_primordial(-7),0.005);
+		Assert.assertEquals(2310, prim.get_primordial(-11),0.005);
+		Assert.assertEquals(30030, prim.get_primordial(-13),0.005);
+		Assert.assertEquals(510510, prim.get_primordial(-17),0.005);
+		Assert.assertEquals(9699690, prim.get_primordial(-19),0.005);
+		Assert.assertEquals(223092870, prim.get_primordial(-23),0.005);
+
+
+	}
+
+}
